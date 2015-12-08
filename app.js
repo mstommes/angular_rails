014a4eb604b962b9ff94ca32e0e6fa79c0753710
[MStommes] <mstommes@gmail.com>
2015-12-07 19:33:41 -0600

    added upvoting function to html and app.js

--- app.js
+++ app.js
@@ -11,11 +11,16 @@ angular.module('flapperNews', [])
 		{title:'post 5', upvotes:4},
 		]
 		$scope.addPost = function(){
+			if(!$scope.title || $scope.title === '') {
+			 return; 
+			}
 			$scope.posts.push({title: $scope.title, upvotes: 0});
-			$scope.title = '';[
-		]
+			$scope.title = '';
+	
 		}
-
+		$scope.incrementUpvotes = function(post) {
+  		post.upvotes += 1;
+			};
 
 
 	}]);
